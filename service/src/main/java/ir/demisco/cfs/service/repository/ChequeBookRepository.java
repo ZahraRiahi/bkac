492f58c3f696faebcdfc068d87b7f877a6454c44
m.kazemi <m.kazemi@istd.com>
2021-12-01 17:15:50 +0330

    feature-fin-726

--- service/src/main/java/ir/demisco/cfs/service/repository/ChequeBookRepository.java
+++ service/src/main/java/ir/demisco/cfs/service/repository/ChequeBookRepository.java
@@ -11,5 +11,12 @@ public interface ChequeBookRepository extends JpaRepository<ChequeBook, Long> {
     @Query("select cb from  ChequeBook cb where cb.chequeBookType.id=:chequeBookTypeId and cb.deletedDate is null")
     List<ChequeBook> findByChequeBookTypeId(Long chequeBookTypeId);
 
+    @Query("select coalesce(COUNT(cb.id),0) from  ChequeBook cb where cb.bankAccount.id=:bankAccountId and cb.numStart=:numStart  and cb.deletedDate is null ")
+    Long findByChequeBookAndBankAccountAndNumStart(Long bankAccountId, Long numStart);
 
+    @Query(value = " SELECT LEVEL LVL " +
+            "            FROM DUAL " +
+            "          CONNECT BY LEVEL <= :numEnd - :numStart + 1 "
+            , nativeQuery = true)
+    List<Long> findByChequeAndNumEndAndNumStart(Long numEnd, Long numStart);
 }
---
+++ service/src/main/java/ir/demisco/cfs/service/repository/ChequeUseTypeRepository.java
@@ -0,0 +1,7 @@
+package ir.demisco.cfs.service.repository;
+
+import ir.demisco.cfs.model.entity.ChequeUseType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChequeUseTypeRepository extends JpaRepository<ChequeUseType, Long> {
+}
