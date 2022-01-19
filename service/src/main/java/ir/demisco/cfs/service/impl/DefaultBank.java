02e4cbf47b76fb5acca10473be0fa6af218b2462
m.kazemi <m.kazemi@istd.com>
2022-01-19 12:12:04 +0330

    feature-fin-1016

--- service/src/main/java/ir/demisco/cfs/service/impl/DefaultBank.java
+++ service/src/main/java/ir/demisco/cfs/service/impl/DefaultBank.java
@@ -10,7 +10,6 @@ import ir.demisco.cloud.core.middle.model.dto.DataSourceRequest;
 import ir.demisco.cloud.core.middle.model.dto.DataSourceResult;
 import ir.demisco.cloud.core.middle.service.business.api.core.GridFilterService;
 import org.springframework.stereotype.Service;
-
 import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.Date;
