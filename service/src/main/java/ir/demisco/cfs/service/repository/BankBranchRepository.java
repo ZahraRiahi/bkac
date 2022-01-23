0dd3a0e37907160e0d1194d13ca4879da2dcb4da
f.ashourinia <1997!Farzan>
2022-01-23 10:58:09 +0330

    FIN-913 number-6

--- service/src/main/java/ir/demisco/cfs/service/repository/BankBranchRepository.java
+++ service/src/main/java/ir/demisco/cfs/service/repository/BankBranchRepository.java
@@ -18,4 +18,14 @@ public interface BankBranchRepository extends JpaRepository<BankBranch, Long> {
 
     @Query("select coalesce(COUNT(br.id),0) from BKACBankBranch br  where br.code=:code and  br.id not in (:id) and br.bank.id=:bankId and br.deletedDate is null ")
     Long getCountBankBranchByCodeAndBankIdAndDeletedDate(String code, Long id, Long bankId);
+
+    @Query(value = "select count(t.id)" +
+            "  from bkac.BANK_BRANCH t" +
+            "  left join bkac.bank_account ba" +
+            "    on t.id = ba.bank_branch_id" +
+            "  left join rprq.request_detail_info rdi" +
+            "    on t.id = rdi.bank_branch_id" +
+            " where t.id  = :bankBranchId" +
+            "   and (ba.bank_branch_id = :bankBranchId or rdi.bank_branch_id = :bankBranchId)", nativeQuery = true)
+    long getCountByIdForDelete(Long bankBranchId);
 }
