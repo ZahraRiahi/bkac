7cd1050f7b8adb916bdd8ee7d8e271100cbb9cba
m.kazemi <m.kazemi@istd.com>
2022-03-29 11:01:31 +0430

    feature-fin-1196

--- model/src/main/java/ir/demisco/cfs/model/entity/BankAccountType.java
+++ model/src/main/java/ir/demisco/cfs/model/entity/BankAccountType.java
@@ -1,6 +1,12 @@
 package ir.demisco.cfs.model.entity;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.Date;
 
